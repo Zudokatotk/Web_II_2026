bc5c0abf5d1e0410ac5bf0a3d2802301a7343308
Zudokatotk <juavesto@gmail.com>
2026-02-23 08:30:06 -0400

    Ingreso desde laptop

--- Unidad_1/repasoJS/9-OperacionesDeComparacion.js
+++ Unidad_1/repasoJS/9-OperacionesDeComparacion.js
@@ -1,19 +1,17 @@
-const valorPasaje=1000
-if(valorPasaje == 1000)
-{
-    console.log(`el pasaje es correcto`);
-}
-const paisDestino="Ecuador";
-const paisesDisponibles = ["Bolvia","ecuador","brasil","vanezuela","italia","francia"];
-
-let edadPasaje=17;
-let acompaniado=true;
-
-console.log(`pasajes para ${paisesDisponibles}`);
-if((paisesDisponibles.indexOf(paisDestino)> -1 && (edadPasaje >=18 || acompaniado)))
-{
-    console.log("pasaje disponible para venta");
-}else
-{
-    console.log("no se puede vender el pasaje");
+const paisDestino = "Argentina";
+const paisesdisponibles = ["Argentina", "Brasil", "Chile", "Peru"];
+let edadpasajero = 17;
+let acompanante = true;
+if (paisesdisponibles.indexOf(paisDestino) !== -1) {
+    if (edadpasajero >= 18) {
+        console.log("Pasaje disponible para venta.");
+    } else {
+        if (acompanante) {
+            console.log("Pasaje disponible para venta.");
+        } else {
+            console.log("Pasaje no disponible para venta.");
+        }
+    }
+} else {
+    console.log("Pasaje no disponible para venta.");
 }
